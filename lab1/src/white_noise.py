52e1a237188bdc27e10946f656b6cd28fd54563d
auperman-lab <121299815+auperman-lab@users.noreply.github.com>
2025-03-18 21:06:54 +0200

    start lab1

---
+++ lab1/src/white_noise.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+lower_limit = 0
+upper_limit = 5
+step = 0.01
+size = int((upper_limit - lower_limit) / step) + 1  # Include last value
+
+samples = np.random.rand(size) - 0.5  # Shift uniform distribution to [-0.5, 0.5]
+
+t = np.linspace(lower_limit, upper_limit, size)  # Create time vector
+
+plt.plot(t, samples)
+plt.grid()
+plt.title('Uniform Noise shifted to [-0.5, 0.5]')
+plt.xlabel('Time (s)')
+plt.ylabel('Noise Signal')
+plt.show()
